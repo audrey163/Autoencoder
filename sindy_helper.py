373495edb9c3208bfb0608cc2e72dc09959e5a6f
audrey163 <62967192+audrey163@users.noreply.github.com>
2022-04-24 18:31:09 -0400

    Update sindy_helper.py

--- sindy_helper.py
+++ sindy_helper.py
@@ -12,13 +12,52 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 
-
+class PolynomialLibrary:
+    def __init__(self, max_degree=params['sindy']['max_degree'], cross_terms=params['sindy']['cross_terms']):
+        self.max_degree = max_degree
+        self.cross_terms = cross_terms
+    def get_candidates(self, dim, feature_names):
+        self.feature_names = feature_names
+        return [self.__polynomial(degree_sequence) 
+                    for degree in range(1,self.max_degree+1)
+                        for degree_sequence in self.__get_degree_sequences(degree, dim)]
+    def __polynomial(self, degree_sequence):
+        def fn(X):
+            terms = torch.stack( tuple(X[:,i]**d for i,d in enumerate(degree_sequence)), axis=1 )
+            return torch.prod(terms, dim=1)
+        fn_name = ' '.join(self.__display_term(self.feature_names[i],d) for i,d in enumerate(degree_sequence) if d)    
+        return (fn, fn_name)   
+    def __display_term(self, feature_name, d):
+        if d == 1:
+            return f'{feature_name}'
+        return f'{feature_name}^{d}'
+    def __get_degree_sequences(self, degree, num_terms):
+        if num_terms == 1:  return [[degree]]
+        if degree == 0:     return [[0 for _ in range(num_terms)]]
+        res = []
+        for d in reversed(range(degree+1)):
+            for seq in self.__get_degree_sequences(degree-d, num_terms-1):
+                res.append([d, *seq])
+        return res
+class TrigLibrary:
+    def __init__(self):
+        self.max_freq = 1
+    def get_candidates(self, dim, feature_names):
+        self.feature_names = feature_names
+        return [trig(i) for trig in [self.__sin, self.__cos] for i in range(dim)]
+    def __sin(self,i):
+        fn = lambda X: torch.sin(X[:,i])
+        fn_name = f'sin({self.feature_names[i]})'
+        return (fn, fn_name)
+    def __cos(self,i):
+        fn = lambda X: torch.cos(X[:,i])
+        fn_name = f'cos({self.feature_names[i]})'
+        return (fn,fn_name)
 class Theta:
-    def __init__(self,num_snapshots, num_features):
-        from regression import PolynomialLibrary, TrigLibrary
-        self.num_snapshots, self.num_features  = num_snapshots , num_features 
+    def __init__(self,):
+        self.num_snapshots, self.num_features  = params['training']['batch_size'], params['architecture']['latent_dimension']
         self.feature_names = [f'x{i+1}' for i in range(self.num_features)]
-        self.candidate_terms = [ lambda x: torch.ones(self.num_snapshots) ]
+        self.candidate_terms = [ lambda x: torch.ones(self.num_snapshots).to(device) ]
         self.candidate_names = ['1']
         self.libs = [ PolynomialLibrary(max_degree=1), TrigLibrary() ]
         for lib in self.libs:
@@ -27,4 +66,4 @@ class Theta:
                 self.candidate_terms.append(term)
                 self.candidate_names.append(name)
     def theta(self,X):
-         return torch.stack(tuple(f(X) for f in self.candidate_terms), axis=1)
+         return torch.stack(tuple(f(X) for f in self.candidate_terms), axis=1).to(device)
